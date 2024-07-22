41bb19aedfe78a1c8c5a46ade30429fedf4dc9ba
ruchigiri <88592765+ruchigiri@users.noreply.github.com>
2024-07-22 16:19:31 -0400

    Add files via upload
    BasicCalculator

---
+++ BasicCalculator/src/com/wipro/project/Calculator.java
@@ -0,0 +1,45 @@
+package com.wipro.project;
+import java.util.Scanner;
+
+class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+    
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error! Division by zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error! Invalid operator.");
+                return;
+        }
+
+        System.out.println("The result is: " + result);
+    }
+}
